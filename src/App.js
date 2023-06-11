34940b5be7db37945825413be2b2739b2b3d3401
Leon Tzu Chiang Chan <leontzuchiangchan@Leons-Air.lan>
2023-06-11 11:06:55 -0400

    change app.js

--- src/App.js
+++ src/App.js
@@ -2,28 +2,9 @@
 // import './App.css';
 import { useState } from "react";
 import Arts from "./Arts";
-import Category from "./Category";
+import Category from "./Categories";
 import items from "./data";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+function App() {}
 
 export default App;
