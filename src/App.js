1d98284782a9ff3526abd84dae3c064e87ba2169
Leon Tzu Chiang Chan <leontzuchiangchan@Leons-Air.lan>
2023-06-15 10:54:55 -0400

    add function

--- src/App.js
+++ src/App.js
@@ -7,6 +7,10 @@ import items from "./data";
 
 const allCategories = ['all', ...new Set(items.map((item) => item.category))]
 
-function App() {}
+function App() {
+  const [menuItems, setMenuItems] = useState(items);
+  // const [categories, setCategories] = useState(allCategories);
+  const [categories] = useState(allCategories);
+}
 
 export default App;
