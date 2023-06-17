362daa7486251bff3bbddca3a6d89330a4cf9f89
Leon Tzu Chiang Chan <leontzuchiangchan@Leons-Air.lan>
2023-06-17 18:07:37 -0400

    add return

--- src/App.js
+++ src/App.js
@@ -20,6 +20,19 @@ function App() {
     const newItems = items.filter((item) => item.category === category);
     setMenuItems(newItems);
   };
+
+  return (
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>our menu</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories categories={categories} filterItems={filterItems} />
+        <Menu items={menuItems} />
+      </section>
+    </main>
+  );
 }
 
 export default App;
