2db4e07ce5513914d9e05484cfc2cfa5792d2452
Leon Tzu Chiang Chan <leontzuchiangchan@Leons-Air.lan>
2023-06-21 12:38:02 -0400

    price -> year

--- src/Arts.js
+++ src/Arts.js
@@ -4,14 +4,14 @@ const Arts = ({ items }) => {
   return (
     <div className='section-center'>
       {items.map((artsItem) => {
-        const { id, name, img, desc, price } = artsItem;
+        const { id, name, img, desc, year } = artsItem;
         return (
           <article key={id} className='arts-item'>
             <img src={img} alt={name} className='photo' />
             <div className='item-info'>
               <header>
                 <h4>{name}</h4>
-                <h4 className='price'>${price}</h4>
+                <h4 className='year'>${year}</h4>
               </header>
               <p className='item-text'>{desc}</p>
             </div>
