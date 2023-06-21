46d6e680e81eff5ce6a6dd2ebf47e5d9fc99b9e2
Leon Tzu Chiang Chan <leontzuchiangchan@Leons-Air.lan>
2023-06-21 12:30:24 -0400

    name change

--- src/Arts.js
+++ src/Arts.js
@@ -4,13 +4,13 @@ const Arts = ({ items }) => {
   return (
     <div className='section-center'>
       {items.map((artsItem) => {
-        const { id, title, img, desc, price } = artsItem;
+        const { id, name, img, desc, price } = artsItem;
         return (
           <article key={id} className='arts-item'>
-            <img src={img} alt={title} className='photo' />
+            <img src={img} alt={name} className='photo' />
             <div className='item-info'>
               <header>
-                <h4>{title}</h4>
+                <h4>{name}</h4>
                 <h4 className='price'>${price}</h4>
               </header>
               <p className='item-text'>{desc}</p>
